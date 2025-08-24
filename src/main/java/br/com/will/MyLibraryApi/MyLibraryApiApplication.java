ae9bffe8056e6e62b10580be52e961c7107bfff9
willUlisses <williamcavalcante06@gmail.com>
2025-08-24 19:57:45 -0300

    Base version commit

---
+++ src/main/java/br/com/will/MyLibraryApi/MyLibraryApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.will.MyLibraryApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyLibraryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyLibraryApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/will/MyLibraryApi/MyLibraryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.will.MyLibraryApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyLibraryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
